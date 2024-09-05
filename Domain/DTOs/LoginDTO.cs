06eee17a81e32d70ee2f0cba4fc3b864f750af2e
elisangeloalves <preto115@gmail.com>
2024-09-05 01:50:23 -0300

    update: aplicando arquitetura clean/onion

---
+++ Domain/DTOs/LoginDTO.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minimal_api.Dominio.DTOs;
+public class LoginDTO 
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
